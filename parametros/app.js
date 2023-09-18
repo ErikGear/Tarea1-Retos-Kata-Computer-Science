349ed644bef5ddf7df4918f831884150f32903b7
ErikGear <erikgalindo23@aragon.unam.mx>
2023-09-17 22:07:40 -0600

    número de parámetros enviados a una función

---
+++ parametros/app.js
@@ -0,0 +1,12 @@
+
+function numeroArgumentos(a, b, c) {
+
+    return arguments.length;
+
+}
+
+
+let argumentos = numeroArgumentos (1,2,3);
+
+console.log(`El numero de argumentos es: ${argumentos}`);
+
